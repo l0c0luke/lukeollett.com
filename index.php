bbbdc3cfd11d3c0c4ab3a534816622b356b32651
Luke Ollett <ollett@gmail.com>
2013-09-16 17:07:37 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Luke Edward Ollett Resume</title>
+<style type="text/css"> 
+<!-- 
+body  {
+	font: 12px Verdana, Arial, Helvetica, sans-serif;
+	background: #fff;
+	margin: 0; /* it's good practice to zero the margin and padding of the body element to account for differing browser defaults */
+	padding: 0;
+	text-align: center; /* this centers the container in IE 5* browsers. The text is then set to the left aligned default in the #container selector */
+	color: #000000;
+	background-color: #FFF;
+}
+
+
+#container { 
+	width: 960px;  /* this width will create a container that will fit in an 800px browser window if text is left at browser default font sizes */
+	margin: 0 auto; /* the auto margins (in conjunction with a width) center the page */
+	text-align: left; /* this overrides the text-align: center on the body element. */
+} 
+
+#sidebar {
+	float: right; 
+	width: 220px; /* since this element is floated, a width must be given */
+	/*background: #EBEBEB; /* the background color will be displayed for the length of the content in the column, but no further */
+	padding: 15px 0; /* top and bottom padding create visual space within this div */
+	text-align: center;
+}
+#sidebar h3, #sidebar p {
+	margin-left: 10px; /* the left and right margin should be given to every element that will be placed in the side columns */
+	margin-right: 10px;
+}
+
+#mainContent {
+	/*margin: 0 13em 0 1.5em; /* the left margin can be given in ems or pixels. It creates the space down the left side of the page. */
+	width: 720px;
+	border-right: 1px solid;
+	padding-right: 20px;
+	text-align:justify;
+}  
+
+.title {
+	font-size: 25px;
+	font-weight: bold;
+	text-align: center;
+}
+.sub_title {
+	font-size: 14px;
+	margin-top: -22px;
+	text-align: center;
+}
+h3 {
+	text-decoration: underline;
+}
+</style>
+</head>
+
+<body>
+
+<div id="container">
+  <div id="sidebar">
+    <h4>Facts About Luke</h4>
+    <p>I have lived in England, California, Chile, and Argentina.</p>
+    <p>I regularly race my bicycle over 100km averaging 42km/h. I have won races in three continents.</p>
+    <p>I have organized and cooked for 70 person dinners.</p>
+    <p>I scored a 690 on the GMAT examination with a perfect score in math and arithmetic.</p>
+    <p>I have a blog www.OurThursday.com that gets 200 readers a day. We regularly do public blog readings.</p>
+    <p>I am in the Phi Kappa Psi fraternity.</p>
+    <p>I have climbed a mountain in Bolivia that was over 20k feet high.</p>
+    <p>I am a dual citizen holding passports from the United Kingdom and the United States.</p>
+    
+    <h4>Programming Languages</h4>
+    <ul style="text-align: left;">
+    	<li>Strong PHP - Used every day</li>
+    	<li>Strong Javascript - Used every day</li>
+    	<li>Medium JAVA - Basis of my university education. Not used since.</li>
+    	<li>Medium Perl - Used daily at IBM. Small usage with server automation scripts still but not often.</li>
+    	<li>Weak Objective C - Small hacks needed for iPhone applications not very regularly.</li>
+    	<li>Weak Python - Used sometimes for server automation tasks.</li>
+    </ul>
+    
+    <h4>Technologies and Frameworks</h4>
+    <ul style="text-align: left;">
+    	<li>Linux user for the last 10 years on both my servers and clients</li>
+    	<li>Apple OSX experience over the last two years</li>
+    	<li>CodeIgniter and MySQL web applications</li>
+    	<li>PhoneGap to deploy mobile applications on many platforms easily</li>
+    	<li>JQTouch - JQuery based mobile applications</li>
+    	<li>Expert SugarCRM</li>
+    	<li>Expert Wordpress</li>
+    	<li>Expert Asterisk VOIP</li>
+    </ul>
+    
+  </div>
+  <div id="mainContent">
+    <p class="title">Luke Edward Ollett</p>
+    <p class="sub_title">ollett@gmail.com | +1.949.371.5292</p>
+    <p></p>
+    
+    <h3>Education</h3>    
+    <p><b>University of California, Irvine</b>
+		<br>Bachelor of Science in Information and Computer Science with a specialization in Networking and Distributed Systems
+		<br>2001 - 2005
+	</p>
+	
+	<p><b>Advanced Spanish</b>
+	<br>I read, write, and speak Spanish at an advanced level.</p>
+	
+	<h3>Work Experience</h3>
+	<p><b>Mobile Gates</b>
+	<br><i>VP of Development Operations
+	<br>www.mobilegates.com
+	<br>Costa Mesa, California | 2011 – Current</i>
+	<ul>
+		<li>Oversee and participate in the technology and software operations of the company.</li>		
+		<li>Manage, implement, and support multiple software projects and their associated teams.</li>
+		<ul>
+			<li>GPS Daily Deals - Android Application, iOS, mobile web, Web</li>
+			<li>Verizon applications. Available from the verizon market place.</li>			
+			<li>
+		</ul>		
+		<li>Administer two-tier server and database architecture for redundancy and replication.</li>				
+		<li>PHP, HTML5, JS/CSS, MySQL, Android Java, Objective C</li>
+	</ul>
+	</p>
+	
+	<p><b>Shiva Media and Varuno Consulting</b>
+	<br><i>Chief Technical Officer
+	<br>www.ShivaMedia.com and www.VarunoConsulting.com
+	<br>Montecito, California | 2008 - Current</i>
+	<ul>
+		<li>Management of off shore development and design teams in India and Chile and being the principal developer and designer.</li>
+		<li>Designed, developed, and support the AppWerks application creation system offered by Shiva Media. 
+		Appwerks provides a user friendly, visually based experience to rapidly turn an idea into an app.</li>
+		<ul>
+			<li>With AppWerks we have released 30 applications that are available in the Android and Apple smart phone market places.
+				In the month of March 2011, our applications received 1.7 million page views equating to 43 thousand unique visitors.
+			<li>CodeIgniter PHP Project with a MySQL database. HTML and Javascript are heavily used for the mobile application logic. 25k lines of code.</li>			
+			<li>Java Android applications and Objective C iPhone applications. Using www.PhoneGap.com.</li>
+		</ul>		
+		<li>www.SugarCRM.com deployments. Help clients understand and adopt an online and more effecient way to manage their business using a bespoke customer relationship management implementation.
+			PHP customizations and bespoke integrations into existing business systems.</li>
+		<li>Customized Wordpress Development</li>
+		<ul>
+			<li>PHP, HTML, and Javascript. E-Commerce.
+			<li>www.wakeupcoffeco.com, www.OurThursday.com, www.DaveGlenn.com, www.MissPBody.com, www.ShivaMedia.com</li>
+		</ul>		
+		<li>Voice Over IP office telephone installations using Asterisk.</li>
+		<li>Linux server administration.</li>
+	</ul>
+	</p>
+	
+	<p><b>Telelogic (purchased by IBM my last year there.)</b>
+	<br><i>Change and Configuration Management Consultant
+	<br>http://www-01.ibm.com/software/awdtools/synergy/
+	<br>Irvine, California | 8/2005 – 8/2008</i>
+	<ul>
+		<li>I was the Bobs from Office Space. My job was to fly around the country each week so I could analyze and improve and implement the internal software and business processes and workflows of some of the worlds largest companies.</li>
+		<li>Panasonic Avionics saved $10 million in one year after my deployment of their parts purchasing business process.</li>
+		<li>Daily Perl coding to automate the IBM Rational Change product.</li>
+		<li>Prepared week long training courses for classes of up to 30 students.</li>
+		<li>Linux Server and Informix Database administration and management.</li>
+		<li>Part time IBM Rational DOORS consultant that specialized in requirements and document management amongst large development teams.</li>
+		<li>Companies worked with: Chrysler, GM, Panasonic, Toyota, Northrop Grumman, NASA, Booze Allen, Channel Advisor, Hamilton Sundstrand, SAIC, Securaplane</li>
+	</ul>
+	</p>
+	
+	<p><b>Expitrans Inc</b>
+	<br><i>Web/System Programmer
+	<br>www.expitrans.com
+	<br>Costa Mesa, California | 1/2005 – 6/2005</i>
+	<ul>
+		<li>PHP merchant services web programming.</li>
+	</ul>
+	</p>
+	
+	<p><b>K. Hovnanian Homes</b>
+	<br><i>IS Administrative Assistant
+	<br>www.khov.com
+	<br>Irvine, California | 9/2003-7/2004</i>
+	<ul>
+		<li>Managed and maintained a 200+ computer network and 200+ phone/fax lines.</li>
+		<li>Citrix Server Administration. </li>
+	</ul>
+	</p>	
+
+	<p><b>University of California, Irvine</b>
+	<br><i>Senior Project - Location Aware VOIP and Voice Recognition Gaming Platform
+	<br>Irvine, California | 9/2004 – 6/2005</i>
+	<ul>
+		<li>A JAVA Motorola phone application that updated a PHP server of your coordinates on earth. 
+			If you then walked into a geographic zone, the "game" would call your phone and read a voice synthesized text. 
+			The game would interpret your voice response and reply accordingly advising you of your next destination or that 
+			you was eaten by the dragon.</li>
+		<li>Responsible for the game calling the phone using a customized Asterisk VOIP installation. 
+			Additionally I was responsible for the voice recognition, which was done using Sphinx and bash scripts.</li> 
+	</ul>
+	</p>
+	
+	<p><b>Hollywoodcenter Studios</b>
+	<br><i>Telecommunications Technician
+	<br>Hollywood, California | 6/1999–10/1999</i>
+	<ul>
+		<li>A summer well spent wiring a newly constructed movie studio with Phone/Coaxial/Fiber/Network/Electricity.</li>
+	</ul>
+	</p>
+
+  </div>
+  
+<br style="clear: both;" />
+</div>
+
+</body>
+</html>
